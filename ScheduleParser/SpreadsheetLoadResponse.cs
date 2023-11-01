d3a5b49a68a2abed16c5ca0bee56d0e2f2679ae5
Gleb <glekar03@mail.ru>
2023-11-01 21:47:44 +0300

    feat: SpreadsheetLoadResponse.cs header fix

--- ScheduleParser/SpreadsheetLoadResponse.cs
+++ ScheduleParser/SpreadsheetLoadResponse.cs
@@ -1,5 +1,5 @@
-// <copyright file="SpreadsheetLoadResponse.cs" company="PlaceholderCompany">
-// Copyright (c) PlaceholderCompany. All rights reserved.
+// <copyright file="SpreadsheetLoadResponse.cs" company="Gleb Kargin">
+// Copyright (c) Gleb Kargin. All rights reserved.
 // </copyright>
 
 namespace ScheduleParser;
